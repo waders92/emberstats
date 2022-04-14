252a701c3e4712233c482e13ce226244f4c33a48
Wade Carlson <wadecarlson@Wades-MBP.attlocal.net>
2022-04-14 17:25:19 -0500

    update router

--- app/router.js
+++ app/router.js
@@ -7,8 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home', {path: '/'});
   this.route('dashboard');
-  this.route('home');
   this.route('new-round');
 });
 
