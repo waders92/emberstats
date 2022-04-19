fd0da427cc2b5164a7e6ad1c24550c932f9e1e37
Wade Carlson <wadecarlson@Wades-MBP.attlocal.net>
2022-04-18 19:25:04 -0500

    split out 9 and 18 filter

--- app/components/card-overview.js
+++ app/components/card-overview.js
@@ -1,4 +1,24 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    nines: null,
+    eighteens: null,
+
+    init() {
+        this._super(...arguments);
+        this.model = this.model.filter(m => {
+            let date = new Date(m.datePlayed);
+            date.setDate(date.getDate() + 1);
+            let dateMonth = date.toLocaleString('default', { month: 'long' });
+            return dateMonth == this.month;
+        });
+
+        this.nines = this.model.filter(m => {
+            return m.holesPlayed == '9';
+        });
+
+        this.eighteens = this.model.filter(m => {
+            return m.holesPlayed == '18';
+        })
+    }
 });
