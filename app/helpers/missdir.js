b5525170dca2e5701685124c684fcbb7e37b252f
Wade Carlson <wadecarlson@Wades-MBP.attlocal.net>
2022-04-22 14:44:29 -0500

    show page update

---
+++ app/helpers/missdir.js
@@ -0,0 +1,26 @@
+import { helper } from '@ember/component/helper';
+
+function missDir(params) {
+  let miss = '';
+  switch (params[0].toLowerCase()) {
+      case 'l':
+        miss = 'Left';
+        break;
+      case 'r':
+        miss = 'Right';
+        break;
+      case 's':
+        miss = 'Short';
+        break;
+       case 'n':
+        miss = 'Long';
+        break;
+       default:
+        miss = dir;
+        break;
+  }
+
+  return miss;
+}
+
+export default helper(missDir);
